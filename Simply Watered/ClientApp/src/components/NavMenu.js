8cf6c6215a89356a1d01a07d662ce6fc7ab8f369
Hryhorii-V <hryhorii.voloshyn@nure.ua>
2022-07-16 10:48:37 +0300

    NavMenu fix

--- Simply Watered/ClientApp/src/components/NavMenu.js
+++ Simply Watered/ClientApp/src/components/NavMenu.js
@@ -1,85 +1,118 @@
-import React, { Component } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import { LoginMenu } from './api-authorization/LoginMenu';
-import './NavMenu.css';
-import authService from './api-authorization/AuthorizeService'
+import React, { Component } from "react";
+import {
+  Collapse,
+  Container,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { LoginMenu } from "./api-authorization/LoginMenu";
+import "./NavMenu.css";
+import authService from "./api-authorization/AuthorizeService";
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
 
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.toggleNavbar = this.toggleNavbar.bind(this);
     this.state = {
       collapsed: true,
       admin: false,
-
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.loadUser();
   }
 
   async loadUser() {
-    const token = await authService.getAccessToken();
-    const response = await fetch(`api/admin`, {
+    try {
+      const token = await authService.getAccessToken();
+      const response = await fetch(`api/admin`, {
         method: "GET",
-        headers: !token ? { 
-            'Content-Type': 'application/json'
-         } : {
-              'Content-Type': 'application/json',
-               'Authorization': `Bearer ${token}` 
+        headers: !token
+          ? {
+              "Content-Type": "application/json",
+            }
+          : {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
             },
-    });
-    const data = await response.json();
-    this.setState({ admin: data});
+      });
+      const data = await response.json();
+      this.setState({ admin: data });
+    } catch {}
   }
 
-  toggleNavbar () {
+  toggleNavbar() {
     this.setState({
-      collapsed: !this.state.collapsed
+      collapsed: !this.state.collapsed,
     });
   }
 
-  render () {
- 
-
+  render() {
     return (
       <>
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/">Simply Watered</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-
-              {!this.state.admin && 
-                <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/regiongroups">Переглянути ділянки</NavLink>
-                </NavItem>
-              }
-                {this.state.admin && 
-                <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/admin/users">Переглянути користувачів</NavLink>
-                </NavItem>
-                }
-                 {this.state.admin && 
-                <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/admin/devices">Переглянути пристрої</NavLink>
-                </NavItem>
-
-                }
-                <LoginMenu>
-                </LoginMenu>
-              </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </header>
+        <header>
+          <Navbar
+            className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3"
+            light
+          >
+            <Container>
+              <NavbarBrand tag={Link} to="/">
+                Simply Watered
+              </NavbarBrand>
+              <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+              <Collapse
+                className="d-sm-inline-flex flex-sm-row-reverse"
+                isOpen={!this.state.collapsed}
+                navbar
+              >
+                <ul className="navbar-nav flex-grow">
+                  {!this.state.admin && (
+                    <NavItem>
+                      <NavLink
+                        tag={Link}
+                        className="text-dark"
+                        to="/regiongroups"
+                      >
+                        Переглянути ділянки
+                      </NavLink>
+                    </NavItem>
+                  )}
+                  {this.state.admin && (
+                    <NavItem>
+                      <NavLink
+                        tag={Link}
+                        className="text-dark"
+                        to="/admin/users"
+                      >
+                        Переглянути користувачів
+                      </NavLink>
+                    </NavItem>
+                  )}
+                  {this.state.admin && (
+                    <NavItem>
+                      <NavLink
+                        tag={Link}
+                        className="text-dark"
+                        to="/admin/devices"
+                      >
+                        Переглянути пристрої
+                      </NavLink>
+                    </NavItem>
+                  )}
+                  <LoginMenu></LoginMenu>
+                </ul>
+              </Collapse>
+            </Container>
+          </Navbar>
+        </header>
       </>
     );
   }
